d96c3dfe5309a81a47d8891340c510459e49b462
sinbi <snbiiiii1024@gmail.com>
2023-04-03 20:37:25 +0900

    http -> dio

--- lib/http_setup.dart
+++ lib/http_setup.dart
@@ -1,19 +1,28 @@
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 const String baseUrl = "http://52.78.111.175:8000"; // 기본 url (고정)
-final http.Client httpClient = http.Client(); // 아 이거 뭐더라 까먹었어요
+// final http.Client httpClient = http.Client(); // 아 이거 뭐더라 까먹었어요
+//
+// Map<String, String> getHeaders() {
+//   // 헤더 설정
+//   return {
+//     'Content-Type': 'application/json',
+//   };
+// }
 
-Map<String, String> getHeaders() {
-  // 헤더 설정
-  return {
-    'Content-Type': 'application/json',
-  };
-}
+// Future<http.Response> post(String url, String body) async {
+//   // post 메소드
+//   final headers = getHeaders();
+//   final response = await httpClient.post(Uri.parse('$baseUrl$url'),
+//       headers: headers, body: body);
+//   return response;
+// }
 
-Future<http.Response> post(String url, String body) async {
-  // post 메소드
-  final headers = getHeaders();
-  final response = await httpClient.post(Uri.parse('$baseUrl$url'),
-      headers: headers, body: body);
+final dio = Dio();
+
+Future<Response> post(String url, String body) async {
+  final response = await dio.post('$baseUrl$url', data: body);
+
   return response;
 }
