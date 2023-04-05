d79753410814635bd882541dec21996d437cbc1b
sinbi <snbiiiii1024@gmail.com>
2023-04-05 23:47:30 +0900

    kiosk 레이아웃

--- lib/common/kiosk_main.dart
+++ lib/common/kiosk_main.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:math';
+import 'dart:ui';
 import 'package:edu_application_pre/http_setup.dart';
-import 'package:edu_application_pre/types.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class KioskMain extends StatefulWidget {
   KioskMain({Key? key}) : super(key: key);
@@ -11,6 +13,11 @@ class KioskMain extends StatefulWidget {
 }
 
 class KioskMainState extends State<KioskMain> {
+  final List<String> backgroundImages = [
+    '../assets/img/background01.jpg',
+    '../assets/img/background02.jpg',
+  ];
+
   static List<dynamic> lectureList = [];
 
   void getLectureList() async {
@@ -29,7 +36,6 @@ class KioskMainState extends State<KioskMain> {
           lectureList.add(lecture);
         }
       }
-      // lectureList = res.data['resultData'];
     }
   }
 
@@ -41,24 +47,186 @@ class KioskMainState extends State<KioskMain> {
 
   @override
   Widget build(BuildContext context) {
+    final random = Random();
     return Scaffold(
         body: Container(
             decoration: BoxDecoration(
                 image: DecorationImage(
                     fit: BoxFit.cover,
-                    image: AssetImage('../assets/img/background01.jpg'))),
+                    image: AssetImage(backgroundImages[
+                        random.nextInt(backgroundImages.length)]))),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(50, 50, 0, 50),
+                      child: Container(
+                          width: 400.0,
+                          height: 260.0,
+                          decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(0.4),
+                            borderRadius: BorderRadius.circular(45),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(right: 20.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(bottom: 20.0),
+                                      child: Image.asset(
+                                        '../assets/img/iconsample01.png',
+                                        width: 96.0,
+                                      ),
+                                    ),
+                                    Text(
+                                      "현재 통합 대기",
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        color:
+                                            Color.fromRGBO(255, 255, 255, 0.6),
+                                      ),
+                                    ),
+                                    Text(
+                                      '나쁨',
+                                      style: TextStyle(
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  Text(
+                                    "실내 대기",
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      color: Color.fromRGBO(255, 255, 255, 0.6),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.fromLTRB(0, 10.0, 0, 20.0),
+                                    child: Text(
+                                      '보통',
+                                      style: TextStyle(
+                                          fontSize: 44.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                  Text.rich(
+                                    TextSpan(
+                                        text: "미세 먼지 ",
+                                        style: TextStyle(
+                                          fontSize: 18.0,
+                                          color: Color.fromRGBO(
+                                              255, 255, 255, 0.5),
+                                        ),
+                                        children: <TextSpan>[
+                                          TextSpan(
+                                            text: "보통(35)",
+                                            style: TextStyle(
+                                                fontSize: 18.0,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold),
+                                          )
+                                        ]),
+                                  ),
+                                  Text.rich(
+                                    TextSpan(
+                                        text: "초미세 먼지 ",
+                                        style: TextStyle(
+                                          fontSize: 18.0,
+                                          color: Color.fromRGBO(
+                                              255, 255, 255, 0.5),
+                                        ),
+                                        children: <TextSpan>[
+                                          TextSpan(
+                                            text: "나쁨(68)",
+                                            style: TextStyle(
+                                                fontSize: 18.0,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold),
+                                          )
+                                        ]),
+                                  )
+                                ],
+                              )
+                            ],
+                          )),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.fromLTRB(0, 43.0, 0, 15.0),
+                              width: 1220.0,
+                              height: 150.0,
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.6),
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              alignment: Alignment.centerLeft,
+                              child: Row(
+                                children: const [
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 60.0),
+                                    child: Icon(
+                                      FontAwesomeIcons.solidBell,
+                                      size: 55,
+                                      color: Color(0xffFFFFFF),
+                                    ),
+                                  ),
+                                  Expanded(
+                                      child: Align(
+                                          alignment: Alignment.center,
+                                          child: Text(
+                                              "06/25~08/01 MIT2 강의실 리모델링 공사 기간",
+                                              style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 36.0,
+                                                fontWeight: FontWeight.w600,
+                                              ))))
+                                ],
+                              ),
+                            ),
+                            Text("QR 인증을 하시려면 스크린을 터치해주세요.",
+                                style: TextStyle(
+                                  color: Color(0xff565656),
+                                  fontSize: 28.0,
+                                  fontWeight: FontWeight.normal,
+                                ))
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
                 Container(
-                    margin: EdgeInsets.symmetric(vertical: 20.0),
                     height: 580.0,
                     child: ListView.builder(
                       scrollDirection: Axis.horizontal,
                       shrinkWrap: true,
                       itemCount: lectureList.length,
                       itemBuilder: (context, index) {
+                        TextStyle myTextStyle = TextStyle(
+                          color: Colors.white,
+                          fontSize: 32.0,
+                          fontWeight: FontWeight.bold,
+                        );
+
                         String room = lectureList[index]['roomName'];
                         String name = lectureList[index]['lectureName'];
                         String startTime = lectureList[index]['startTime']
@@ -71,38 +239,102 @@ class KioskMainState extends State<KioskMain> {
 
                         return Stack(children: [
                           Container(
-                              width: 330,
-                              height: 580,
-                              margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                              width: 380,
+                              height: 550,
+                              margin: EdgeInsets.only(left: 50),
                               decoration: BoxDecoration(
-                                color: Colors.white.withOpacity(0.5),
+                                color: Colors.white.withOpacity(0.4),
                                 borderRadius: BorderRadius.circular(30),
-                              ),
-                              child: Column(
-                                children: [
-                                  Text(
-                                    room,
-                                  ),
-                                  Text(
-                                    name,
-                                  ),
-                                  Text(
-                                    startTime,
-                                  ),
-                                  Text(
-                                    '$teacher 강사님',
-                                  ),
-                                  Text(
-                                    '출석 표시',
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Color(0xff000000).withOpacity(0.1),
+                                    blurRadius: 30,
+                                    offset: Offset(
+                                        0, 0), // changes position of shadow
                                   ),
                                 ],
+                              ),
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(room,
+                                        overflow: TextOverflow.ellipsis,
+                                        textAlign: TextAlign.center,
+                                        style: myTextStyle.copyWith(
+                                            fontSize: 60.0)),
+                                    Container(
+                                      margin: EdgeInsets.fromLTRB(0, 30, 0, 30),
+                                      width: 100,
+                                      height: 1, //// 선의 높이를 지정합니다
+                                      color: Colors.white, // 선의 색상을 지정합니다
+                                    ),
+                                    Text(name,
+                                        overflow: TextOverflow.ellipsis,
+                                        textAlign: TextAlign.center,
+                                        style: myTextStyle.copyWith(
+                                            fontSize: 48.0)),
+                                    Padding(
+                                      padding:
+                                          EdgeInsets.fromLTRB(0, 20, 0, 20),
+                                      child: Text('$startTime 에 시작',
+                                          style: myTextStyle.copyWith(
+                                              fontSize: 28.0,
+                                              fontWeight: FontWeight.normal)),
+                                    ),
+                                    Text.rich(
+                                      textAlign: TextAlign.center,
+                                      TextSpan(
+                                        text: teacher,
+                                        style: myTextStyle,
+                                        children: <TextSpan>[
+                                          TextSpan(
+                                            text: ' 강사님',
+                                            style: myTextStyle.copyWith(
+                                                fontWeight: FontWeight.normal),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(top: 30.0),
+                                      child: Text.rich(
+                                        textAlign: TextAlign.center,
+                                        TextSpan(
+                                          text: '출석 ',
+                                          style: myTextStyle,
+                                          children: <TextSpan>[
+                                            TextSpan(
+                                              text: '듣는 애들',
+                                              style: myTextStyle.copyWith(
+                                                  fontSize: 36.0),
+                                            ),
+                                            TextSpan(
+                                              text: ' / 전체 인',
+                                              style: myTextStyle.copyWith(
+                                                  fontWeight:
+                                                      FontWeight.normal),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               )),
                         ]);
                         // child: Text(
                         //   name,
                         // ));
                       },
-                    ))
+                    )),
+                Container(
+                  child: Image.asset(
+                    'assets/img/whitelogo.png',
+                  ),
+                  width: 170.0,
+                ),
               ],
             )));
   }
